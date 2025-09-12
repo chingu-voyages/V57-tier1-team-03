ac2c32716db8bd11b481c592de6828defafd3633
Tiffany Nebo <nebotiffany@gmail.com>
2025-09-12 16:53:17 +0100

    chore: Set global styling and remove defaults

--- pr-status-board/src/App.jsx
+++ pr-status-board/src/App.jsx
@@ -1,26 +1,11 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>PRs Status Board</h1>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <p>Development begins!</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
