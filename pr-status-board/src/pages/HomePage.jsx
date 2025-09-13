1cf62a670e4a08956f97e2e463fc772afda5b972
abbey eleazer <abbeyeleazer@gmail.com>
2025-09-13 01:40:12 +0000

    add pages

---
+++ pr-status-board/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <img src="src/assets/logo.png" alt="logo" />
+    </div>
+  )
+}
+
+export default Home
---
+++ pr-status-board/src/pages/OpenPRsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OpenPRs = () => {
+  return (
+    <div>OpenPRs</div>
+  )
+}
+
+export default OpenPRs
