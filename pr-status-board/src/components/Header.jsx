8c37efa9210d245fcef5b8e4e1a84c38143b3a7f
abbey eleazer <abbeyeleazer@gmail.com>
2025-09-17 09:16:55 +0000

    header menu

--- pr-status-board/src/components/Header.jsx
+++ pr-status-board/src/components/Header.jsx
@@ -1,27 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
+
+
+  
+
   return (
-    <div className={styles.container}>
-      <nav className={styles.header}>
-        <ul className={styles.ul}>
-          <li>
-            <Link to="/">Home</Link>
+    <>
+      <div className={styles.container}>
+        <nav className={styles.header}>
+          <ul className={styles.ul}>
+            <li className={styles.link}>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className={styles.link}>
+              <NavLink to="/open-prs">Open PRs</NavLink>
+            </li>
+            <li className={styles.link}>
+              <NavLink
+               
+                to="/closed-prs"
+              >
+                Closed PRs
+              </NavLink>
+            </li>
+          </ul>
+          <div>
+            <img className={styles.logo} src="src/assets/logo.png" alt="logo" />
+          </div>
+          {/* <div >X</div> */}
+        </nav>
+      </div>
+
+      {/* Modal Menu  */}
+
+      {/* <div>
+        <ul className={styles.modal_menu}>
+          <li className={styles.link}>
+            <NavLink to="/">Home</NavLink>
           </li>
-          <li>
-            <Link to="/open-prs">Open PRs</Link>
+          <li className={styles.link}>
+            <NavLink to="/open-prs">Open PRs</NavLink>
           </li>
-          <li>
-            <Link to="/closed-prs">Closed PRs</Link>
+          <li className={styles.link}>
+            <NavLink to="/closed-prs">Closed PRs</NavLink>
           </li>
         </ul>
-        <div>
-          <img className={styles.logo} src="src/assets/logo.png" alt="logo" />
-        </div>
-      </nav>
-    </div>
+      </div> */}
+    </>
   );
 };
 
