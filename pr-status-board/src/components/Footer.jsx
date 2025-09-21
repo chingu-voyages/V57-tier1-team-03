72deb8d07d5c9b8905b2197220a94840ef81a364
Eleazer Abbey <134316973+abbey-eleazer@users.noreply.github.com>
2025-09-21 00:53:21 +0000

    Revert "Feat/header and footer"

--- pr-status-board/src/components/Footer.jsx
+++ pr-status-board/src/components/Footer.jsx
@@ -1,24 +1,10 @@
-import React from "react";
-import styles from "./Footer.module.css";
+import React from 'react'
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.footer}>
-        <img className={styles.logo} src="src/assets/logo.png" alt="logo" />
-        <div className={styles.chatbot_bg}>
-          <img
-            className={styles.chatbot}
-            src="src/assets/WappGPT - logo.png"
-            alt="chat-bot"
-          />
-        </div>
-      </div>
-      <div className={styles.copy}>
-        <p>&copy;2025 All rights reserved.</p>
-      </div>
-    </div>
-  );
-};
+    <div className={styles.footer}>Footer</div>
+  )
+}
 
-export default Footer;
+export default Footer
