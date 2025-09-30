10658636d70afa15f19ed02ca58b821690fec838
abbey eleazer <abbeyeleazer@gmail.com>
2025-09-30 21:45:33 +0000

    make responsive

--- pr-status-board/src/components/Navigation/Header.jsx
+++ pr-status-board/src/components/Navigation/Header.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 import Logo from "/src/assets/logo.png";
 
 const Header = () => {
- 
+
   return (
     <>
       <div className={styles.container}>
@@ -20,27 +19,8 @@ const Header = () => {
               <NavLink to="/closed-prs">Closed PRs</NavLink>
             </li>
           </ul>
-          <div className={styles.mobile_nav}>
             <img className={styles.logo} src={Logo} alt="logo" />
-            <div className={styles.icon}>X</div>
-          </div>
         </nav>
-      </div>
-
-      {/* Modal Menu  */}
-
-      <div>
-        <ul className={styles.modal_menu}>
-          <li className={styles.link}>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li className={styles.link}>
-            <NavLink to="/open-prs">Open PRs</NavLink>
-          </li>
-          <li className={styles.link}>
-            <NavLink to="/closed-prs">Closed PRs</NavLink>
-          </li>
-        </ul>
       </div>
     </>
   );
